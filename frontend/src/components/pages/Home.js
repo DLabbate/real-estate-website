d8127c6ca198b3c9430b68300e0cbe8ba0546f21
Domenic Labbate <domeniclabbate@gmail.com>
2021-05-09 01:15:36 -0400

    styling

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
         className="welcome__image"
       />
       <h1 className="welcome__text">Find your dream home.</h1>
+      <button className="welcome__btn">Start Browsing</button>
     </div>
   );
 };
