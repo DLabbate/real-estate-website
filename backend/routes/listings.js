5fd2bde0e3c710d38d4ddc5e19b5dfdeed9acb7b
Domenic Labbate <domeniclabbate@gmail.com>
2021-05-31 18:31:26 -0400

    Added functionality for deleting a listing

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -26,4 +26,20 @@ router.post("/", authentication, (req, res, next) => {
     });
 });
 
+router.delete("/:listingId", authentication, (req, res, next) => {
+  const listingId = req.params.listingId;
+
+  Listing.deleteOne({ _id: listingId })
+    .exec()
+    .then((result) =>
+      res
+        .status(200)
+        .json({ message: "Listing deleted" })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json({ error: err });
+        })
+    );
+});
+
 module.exports = router;
