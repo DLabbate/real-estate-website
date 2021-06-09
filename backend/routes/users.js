691732a70a7546114c0207bf25170d169c453967
Domenic Labbate <domeniclabbate@gmail.com>
2021-06-09 18:12:11 -0400

    Removed unused imports

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const authentication = require("../middleware/authentication");
 const userController = require("../controllers/user-controller");
 require("dotenv").config();
