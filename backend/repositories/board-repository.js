acd71f2a7ed2570830632bc9c97d7887cd289365
Domenic Labbate <domeniclabbate@gmail.com>
2021-06-17 22:09:22 -0400

    Now populate board with listing info

--- backend/repositories/board-repository.js
+++ backend/repositories/board-repository.js
@@ -1,6 +1,19 @@
 const mongoose = require("mongoose");
 const Board = require("../models/board");
 
+const populateQuery = {
+  path: "columns",
+  populate: {
+    path: "items",
+    model: "Note",
+    select: "-__v",
+    populate: {
+      path: "listing",
+      model: "Listing",
+      select: "-__v",
+    },
+  },
+};
 /**
  * Creates a new board. Only 1 per user.
  */
@@ -35,13 +48,17 @@ exports.createNewBoard = async (userId) => {
  * Finds a board by userId
  */
 exports.findBoardByUserId = async (userId) => {
-  return await Board.findOne({ user: userId }).select("-__v").exec();
+  return await Board.findOne({ user: userId })
+    .select("-__v")
+    .populate(populateQuery)
+    .exec();
 };
 
 exports.editBoard = async (userId, newBoard) => {
   const board = await Board.findOneAndUpdate({ user: userId }, newBoard, {
     new: true,
   })
+    .populate(populateQuery)
     .select("-__v")
     .exec();
   return board.toObject();
