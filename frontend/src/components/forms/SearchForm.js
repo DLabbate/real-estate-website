76ff25264c59a3d1784d9086039cacbe87d7260c
Domenic Labbate <domeniclabbate@gmail.com>
2021-06-21 00:19:22 -0400

    Refactored SearchForm to use basic state instead of formik

--- frontend/src/components/forms/SearchForm.js
+++ frontend/src/components/forms/SearchForm.js
@@ -1,99 +1,108 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import "./Form.css";
+import React, { useEffect, useState } from "react";
 import "./SearchForm.css";
-import Button from "../shared/Button";
-import * as Yup from "yup";
-import * as listingApi from "../../utils/api/listing-api";
 import update from "immutability-helper";
 import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import AddressInput from "./AddressInput";
 
 const SearchForm = () => {
-  const initialValues = {
+  const [filterParams, setFilterParams] = useState({
     address: "",
     coordinates: { lat: null, lng: null },
-    radius: Number.MAX_SAFE_INTEGER, // in km
-    minPrice: 0,
-    maxPrice: 999999999,
+    radius: 5, // in km
+    minPrice: "",
+    maxPrice: "",
+  });
+
+  const setField = async (fieldName, newValue) => {
+    const newFilterParams = update(filterParams, {
+      [fieldName]: { $set: newValue },
+    });
+    setFilterParams(newFilterParams);
   };
 
-  const validate = Yup.object({
-    address: Yup.string()
-      .max(80, "Must be 80 characters or less")
-      .required("Address is required")
-      .test(
-        "location",
-        "Please select a valid address from the list",
-        function () {
-          const { lat, lng } = this.parent["coordinates"];
-          if (lat && lng) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      ),
-    minPrice: Yup.number()
-      .max(999999999, "Exceeded max value of $999,999,999")
-      .positive()
-      .integer(),
-    maxPrice: Yup.number()
-      .max(999999999, "Exceeded max value of $999,999,999")
-      .positive()
-      .integer(),
+  const onChangeAddress = async (address) => {
+    setField("address", address);
+  };
+
+  const onSelectAddress = async (address) => {
+    console.log("Selected the following address: ", address);
+    setField("address", address);
+  };
+
+  const onChangeRadius = async (event) => {
+    setField("radius", event.target.value);
+  };
+
+  const onChangeMinPrice = async (event) => {
+    setField("minPrice", event.target.value);
+  };
+
+  const onChangeMaxPrice = async (event) => {
+    setField("maxPrice", event.target.value);
+  };
+
+  const updateCoordinates = async (address) => {
+    if (filterParams.address) {
+      const results = await geocodeByAddress(address);
+      const coordinates = await getLatLng(results[0]);
+      console.log(coordinates);
+
+      //Update coordinates
+      setField("coordinates", coordinates);
+    }
+  };
+
+  /**
+   * Every time the address changes, update the coordinates
+   */
+  useEffect(() => {
+    updateCoordinates(filterParams.address);
+  }, [filterParams.address]);
+
+  useEffect(() => {
+    console.log("Filter Params: ", filterParams);
   });
 
   return (
-    // <div className="filter">
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validate}
-      onSubmit={() => {}}
-    >
-      {({ isSubmitting, setFieldValue, values }) => (
-        <Form className="filter">
-          <div className="filter__row">
-            {/* <input placeholder="Location" className="filter__input"></input> */}
-            <AddressInput
-              value={values.address}
-              onChange={(address) => {
-                setFieldValue("address", address);
-              }}
-              onSelect={async (address) => {
-                console.log("Selected the following address: ", address);
-                setFieldValue("address", address);
-                const results = await geocodeByAddress(address);
-                const coordinates = await getLatLng(results[0]);
-                console.log(coordinates);
-                setFieldValue("coordinates", coordinates);
-              }}
-              suggestionContainerAbsolute={true}
-            />
-          </div>
-          <div className="filter__row">
-            <label className="filter__slider-label">Radius (10km)</label>
-            <input
-              type="range"
-              className="filter__slider"
-              min={1}
-              max={10}
-              step={1}
-              onChange={(event) => {
-                //console.log(event.target.value);
-                setFieldValue("radius", event.target.value);
-              }}
-            ></input>
-          </div>
-          <div className="filter__row">
-            <input className="filter__input" placeholder="Min. Price"></input>
-            <p>-</p>
-            <input className="filter__input" placeholder="Max. Price"></input>
-          </div>
-        </Form>
-      )}
-    </Formik>
-    // </div>
+    <div className="filter">
+      <div className="filter__row">
+        <AddressInput
+          value={filterParams.address}
+          onChange={onChangeAddress}
+          onSelect={onSelectAddress}
+          suggestionContainerAbsolute={true}
+        />
+      </div>
+      <div className="filter__row">
+        <label className="filter__slider-label">
+          Radius ({filterParams.radius}km)
+        </label>
+        <input
+          type="range"
+          className="filter__slider"
+          value={filterParams.radius}
+          min={1}
+          max={10}
+          step={1}
+          onChange={onChangeRadius}
+        ></input>
+      </div>
+      <div className="filter__row">
+        <input
+          value={filterParams.minPrice}
+          className="filter__input"
+          placeholder="Min. Price"
+          onChange={onChangeMinPrice}
+        ></input>
+        <p>-</p>
+        <input
+          value={filterParams.maxPrice}
+          className="filter__input"
+          placeholder="Max. Price"
+          onChange={onChangeMaxPrice}
+        ></input>
+      </div>
+    </div>
   );
 };
 
