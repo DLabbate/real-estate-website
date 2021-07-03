6af1ac69aba804322a43b026526f9b485be1db03
Domenic Labbate <domeniclabbate@gmail.com>
2021-07-02 23:56:09 -0400

    Added onClick functionality to logo

--- frontend/src/components/shared/Logo.js
+++ frontend/src/components/shared/Logo.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Logo.css";
 import { ReactComponent as LogoSvg } from "../../assets/logo/logo.svg";
+import { useHistory } from "react-router-dom";
 
 const Logo = ({ size }) => {
+  let history = useHistory();
   return (
-    <div className={"logo"}>
+    <div
+      className={"logo"}
+      onClick={() => {
+        history.push("/home");
+        // Refresh page
+        history.go(0);
+      }}
+    >
       <LogoSvg
         className={
           size === "small" ? "logo__icon logo__icon--small" : "logo__icon"
