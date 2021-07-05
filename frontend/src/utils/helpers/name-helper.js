40f6d09085821b985185610d3859b826ca2017cc
Domenic Labbate <domeniclabbate@gmail.com>
2021-07-05 18:14:07 -0400

    Added comments to helper methods

--- frontend/src/utils/helpers/name-helper.js
+++ frontend/src/utils/helpers/name-helper.js
@@ -1,5 +1,7 @@
 /**
  * Formats a name (e.g. firstName, lastName) by removing all numeric characters
+ * @param {string} stringValue - The name to be formatted (e.g. "Domenic123")
+ * @returns {string} The formatted name (e.g. "Domenic")
  */
 export const formatName = (stringValue) => {
   // Strip of all numeric characters
